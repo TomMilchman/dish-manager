a778b23ff097b8b7403d7beea7d1fa330c209100
Tom Milchman <tom.milchman@gmail.com>
2025-07-18 13:27:32 +0300

    Add NODE_ENV check to conditionally log server startup message

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 const app = require("./app.js");
+const isProduction = process.env.NODE_ENV === "production";
 const { PORT, HOST_URL } = require("./config/config");
 const logger = require("./utils/logger");
 
 app.listen(PORT, () => {
-    logger.log("server", `Server is listening on ${HOST_URL}`);
+    if (!isProduction)
+        logger.log("server", `Server is listening on ${HOST_URL}`);
 });
